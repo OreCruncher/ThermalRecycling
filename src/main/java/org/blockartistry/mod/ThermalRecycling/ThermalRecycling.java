716fea9e0df39ec8c73dcfe649074a44513af8e9
OreCruncher <tedjkaiser@gmail.com>
2015-04-17 19:43:45 -0700

    Updated website URL in mcmod.info and bumped revision.

--- src/main/java/org/blockartistry/mod/ThermalRecycling/ThermalRecycling.java
+++ src/main/java/org/blockartistry/mod/ThermalRecycling/ThermalRecycling.java
@@ -44,7 +44,7 @@ public final class ThermalRecycling {
 	public static ThermalRecycling instance = new ThermalRecycling();
 	public static final String MOD_ID = "recycling";
 	public static final String MOD_NAME = "Thermal Recycling";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 	public static final String DEPENDENCIES = "required-after:ThermalExpansion";
 
 	private ModOptions options = new ModOptions();
