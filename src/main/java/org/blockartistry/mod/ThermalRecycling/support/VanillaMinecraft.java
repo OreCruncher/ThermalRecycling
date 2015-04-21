3793699d79cbcbefda551d36947b4d75078d37c0
OreCruncher <tedjkaiser@gmail.com>
2015-04-21 12:31:49 -0700

    Fixed up source license info

--- src/main/java/org/blockartistry/mod/ThermalRecycling/support/VanillaMinecraft.java
+++ src/main/java/org/blockartistry/mod/ThermalRecycling/support/VanillaMinecraft.java
@@ -1,8 +1,7 @@
 /*
- * This file is part of ModpackInfo, licensed under the MIT License (MIT).
+ * This file is part of ThermalRecycling, licensed under the MIT License (MIT).
  *
  * Copyright (c) OreCruncher
- * Copyright (c) contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
