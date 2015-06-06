a38d71637d98f0864b7d0ea68af033fd8d8eb711
OreCruncher <tedjkaiser@gmail.com>
2015-06-06 13:42:21 -0700

    Mod Support: Applied Energistics2

--- src/main/java/org/blockartistry/mod/ThermalRecycling/support/SupportedMod.java
+++ src/main/java/org/blockartistry/mod/ThermalRecycling/support/SupportedMod.java
@@ -86,6 +86,8 @@ public enum SupportedMod {
 	RFWINDMILLS("RF Windmills", "rfwindmill", ModRFWindmills.class),
 	
 	REDSTONE_ARMORY("Redstone Armory", "RArm", ModRedstoneArmory.class),
+	
+	APPLIED_ENERGISTICS("Applied Energistics2", "appliedenergistics2", ModAppliedEnergistics2.class),
 
 	// This is last. Reason is that the plugins have the first crack
 	// at recipes and setting up the necessary black list entries
