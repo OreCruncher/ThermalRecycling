62a7cf8c1093b24c738f58874e599e7fbca802e2
OreCruncher <tedjkaiser@gmail.com>
2016-01-07 06:22:29 -0800

    Turn off autodetect

--- src/main/java/org/blockartistry/mod/ThermalRecycling/data/AutoDetect.java
+++ src/main/java/org/blockartistry/mod/ThermalRecycling/data/AutoDetect.java
@@ -38,7 +38,7 @@ import net.minecraft.item.ItemStack;
 
 public final class AutoDetect {
 	
-	private static boolean EXECUTE = true;
+	private static boolean EXECUTE = false;
 
 	private AutoDetect() {
 	}
